95f6b5ccf7018eb21f50acd47caa06d51d715e29
David Wadden <dwadden@pivotal.io>
2018-09-12 22:48:39 -0500

    Bootstraps Gradle project from Spring Initializr

---
+++ src/main/java/io/pivotal/cnde/portalspike/PortalSpikeApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.cnde.portalspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalSpikeApplication.class, args);
+	}
+}
