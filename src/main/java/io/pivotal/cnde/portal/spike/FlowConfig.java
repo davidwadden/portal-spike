d53c1f1fc5da1029fc283ffb8898e94d479441f1
David Wadden <dwadden@pivotal.io>
2018-09-13 19:41:54 -0500

    Sends message thru broker instead of shunting

--- src/main/java/io/pivotal/cnde/portal/spike/FlowConfig.java
+++ src/main/java/io/pivotal/cnde/portal/spike/FlowConfig.java
@@ -1,18 +1,23 @@
 package io.pivotal.cnde.portal.spike;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.channel.MessageChannels;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
+import org.springframework.messaging.*;
 
 @Configuration
 @EnableBinding(FlowConfig.Cohorts.class)
 public class FlowConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(FlowConfig.class);
 
     @Bean
     public MessageChannel cohortChannel() {
@@ -20,27 +25,31 @@ public class FlowConfig {
     }
 
     @Bean
-    public IntegrationFlow cohortProvisionRequestFlow() {
+    public IntegrationFlow cohortProvisionRequestFlow(@Qualifier("cohort-provision-request") MessageChannel provisionRequest) {
         return IntegrationFlows.from(cohortChannel())
                 .log("cohort-provision-request")
-                .channel(Cohorts.PROVISION)
+                .channel(provisionRequest)
                 .get();
     }
 
     @Bean
-    public IntegrationFlow cohortProvisionResponseFlow() {
-        return IntegrationFlows.from(Cohorts.PROVISION)
+    public IntegrationFlow cohortProvisionResponseFlow(@Qualifier("cohort-provision-response") SubscribableChannel provisionResponse) {
+        return IntegrationFlows.from(provisionResponse)
                 .log("cohort-provision-response")
-                .handle(message -> System.out.println(message.getPayload().toString()))
+                .handle(message -> logger.info("handling message: {}", message.getPayload()))
                 .get();
     }
 
     interface Cohorts {
 
-        String PROVISION = "cohort-provision";
+        String PROVISION_REQUEST = "cohort-provision-request";
+        String PROVISION_RESPONSE = "cohort-provision-response";
 
-        @Input(PROVISION)
-        SubscribableChannel provision();
+        @Output(PROVISION_REQUEST)
+        MessageChannel provisionRequest();
+
+        @Input(PROVISION_RESPONSE)
+        SubscribableChannel provisionResponse();
     }
 
 }
